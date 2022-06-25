4eb213416d49f0442425a01d9f50b88de2434399
aldok <aldo.santos.oliveira95@gmail.com>
2022-06-25 17:40:08 -0600

    Praticando

---
+++ src/Pratica01/Pratica01.java
@@ -0,0 +1,20 @@
+package Pratica01;
+
+import java.util.Scanner;
+
+public class Pratica01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor");
+        int valor01 = sc.nextInt();
+        System.out.println("Digite o segundo valor");
+        int valor02 = sc.nextInt();
+        int valor03 = valor01 + valor02;
+        System.out.println("Resultado " + valor03);
+
+
+
+
+
+    }
+}
