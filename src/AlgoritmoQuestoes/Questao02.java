4339872a97ded57db220701fcb97f5cc9eb2dc0a
aldok <aldo.santos.oliveira95@gmail.com>
2022-06-27 13:45:22 -0300

    Reviando

--- src/AlgoritmoQuestoes/Questao02.java
+++ src/AlgoritmoQuestoes/Questao02.java
@@ -9,12 +9,20 @@ import java.util.Scanner;
 public class Questao02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1,num2,num3,num4, resuladoAdicao;
+        int num1,num2,num3,num4, resuladoAdicao, resultadoMultiplicacao;
+
+        System.out.println("Informe primeiro numero");
+        num1 = sc.nextInt();
+        System.out.println("Informe Segundo numero");
+        num2 = sc.nextInt();
+        System.out.println("Informe Terceiro numero");
+        num3 = sc.nextInt();
+        System.out.println("Informe Quarto numero");
+        num4 = sc.nextInt();
         resuladoAdicao = num1 + num2 + num3 + num4;
-
-
-
-
+        resultadoMultiplicacao = num1 * num2 * num3 * num4;
+        System.out.println("Resultado Adicao :" + resuladoAdicao);
+        System.out.println("Resulado Multiplicacao :" +resultadoMultiplicacao);
 
 
     }
