9a8437c87088ef40530dfd69778df947fc882b58
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-06 23:42:00 -0300

    Reviando

---
+++ src/Pratica01/Pratica10.java
@@ -0,0 +1,10 @@
+package Pratica01;
+
+public class Pratica10 {
+
+    public static void main(String[] args) {
+        for (int pessoa = 0; 0 < 100; pessoa++){
+            System.out.println("Resultado");
+        }
+    }
+}
