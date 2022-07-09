78bd415fc4295757796048de378a00561e0c019d
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-09 18:42:43 -0300

    test

---
+++ src/Pratica01/Pratica12.java
@@ -0,0 +1,26 @@
+package Pratica01;
+
+public class Pratica12 {
+    public static void main(String[] args) {
+        int dia = 4;
+        switch (dia){
+            case 1:
+                System.out.println("DIA 1");
+                break;
+            case 2:
+                System.out.println("Dia 2");
+                break;
+            case 3:
+                System.out.println("DIA 3");
+                break;
+            case 4:
+                System.out.println("Dia  4");
+                break;
+            default:
+                System.out.println("dia invalido");
+
+
+        }
+
+    }
+}
