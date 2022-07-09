d487b9d5910d3869591fd0cfc11be2305a47c5d1
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-08 23:18:33 -0300

    Reviando

--- src/Pratica01/Pratica11.java
+++ src/Pratica01/Pratica11.java
@@ -1,7 +1,17 @@
 package Pratica01;
 
+import java.util.Scanner;
+
 public class Pratica11 {
     public static void main(String[] args) {
-        System.out.println("Revisando");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Valor 1");
+        int valor1 = sc.nextInt();
+        System.out.println("Valor 2");
+        int valor2 = sc.nextInt();
+        int valor3 = valor1 + valor2;
+        System.out.println(valor3);
+
     }
 }
+
