f0a06c56c6e77cadfce58a577f5fc895332e0a3f
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-11 22:42:04 -0300

    test

--- src/Pratica01/Pratica13.java
+++ src/Pratica01/Pratica13.java
@@ -2,10 +2,12 @@ package Pratica01;
 
 public class Pratica13 {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        System.out.println("Resultado" + c);
 
-        int dia;
-
-        switch (dia)
-        }
     }
+
+
 }
