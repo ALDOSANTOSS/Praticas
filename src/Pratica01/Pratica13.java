1c29eba96c6b1d04d5b276410719f783440988fe
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-13 15:32:52 -0300

    Revisando switch

--- src/Pratica01/Pratica13.java
+++ src/Pratica01/Pratica13.java
@@ -1,16 +1,26 @@
 package Pratica01;
 
+import java.util.Scanner;
+
 public class Pratica13 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String dia;
-        switch (dia){
-            c
+        int valor = sc.nextInt();
+        switch (valor){
+            case 1:
+                System.out.println("Valor 1");
+                break;
+            case 2:
+                System.out.println("Valor 2");
+                break;
+            case 3:
+                System.out.println("Valor 3");
+                break;
+            default:
+                System.out.println("Valor invalido");
         }
-        
     }
-
-
 
 }
 
