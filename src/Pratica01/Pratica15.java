b305ce003a86b084b188bff80bf4b440523fb640
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-15 22:31:44 -0300

    Revisando switch

---
+++ src/Pratica01/Pratica15.java
@@ -0,0 +1,25 @@
+package Pratica01;
+
+import java.util.Scanner;
+
+public class Pratica15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade = sc.nextInt();
+
+        switch (idade){
+            case 1:
+                System.out.println("Idade 1");
+                break;
+            case 2:
+                System.out.println("Idade 2");
+                break;
+            case 3:
+                System.out.println("Idade 3");
+                break;
+            default:
+                System.out.println("Idade invalidade");
+
+        }
+    }
+}
