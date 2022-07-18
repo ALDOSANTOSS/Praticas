9d1e10d96b7291685fe6d8b800b3ca0970504202
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-17 23:39:42 -0300

    Revisando switch

---
+++ src/Pratica01/Pratica17.java
@@ -0,0 +1,24 @@
+package Pratica01;
+
+import java.util.Scanner;
+
+public class Pratica17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int dia = sc.nextInt();
+        switch (dia){
+            case 1:
+                System.out.println("Dia 1");
+                break;
+            case 2:
+                System.out.println("Dia 2");
+                break;
+            case 3:
+                System.out.println("Dia 3");
+                break;
+            default:
+                System.out.println("Dia invalido");
+        }
+    }
+}
