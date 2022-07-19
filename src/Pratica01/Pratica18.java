f1ac8ce9427bd3e9c01bb3702dff065e168e8ce5
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-18 22:51:09 -0300

    Revisando switch

---
+++ src/Pratica01/Pratica18.java
@@ -0,0 +1,11 @@
+package Pratica01;
+
+import java.util.Scanner;
+
+public class Pratica18 {
+    public static void main(String[] args) {
+        for(int pessoa = 0;pessoa < 10; pessoa++){
+            System.out.println(pessoa);
+        }
+    }
+}
