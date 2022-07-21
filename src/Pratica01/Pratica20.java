c02e840e6e7f9457605e877ce7f8c750ffb77267
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-20 23:26:38 -0300

    Revisando switch

---
+++ src/Pratica01/Pratica20.java
@@ -0,0 +1,22 @@
+package Pratica01;
+
+public class Pratica20 {
+    public static void main(String[] args) {
+        for(int pessoa = 0; pessoa < 10; pessoa++){
+            System.out.println("resultado"+pessoa);
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
