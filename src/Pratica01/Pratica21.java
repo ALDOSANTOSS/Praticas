bf17862879bd14424db1d417b7d8d3678ee22abb
aldok <aldo.santos.oliveira95@gmail.com>
2022-07-21 22:56:39 -0300

    Revisando switch

---
+++ src/Pratica01/Pratica21.java
@@ -0,0 +1,9 @@
+package Pratica01;
+
+public class Pratica21 {
+    public static void main(String[] args) {
+        for (int numero = 0; numero < 10; numero++){
+            System.out.println(numero);
+        }
+    }
+}
