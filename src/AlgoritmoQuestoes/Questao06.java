18daef0e51692ef69449900a7d52701196982f5d
aldok <aldo.santos.oliveira95@gmail.com>
2022-08-17 23:35:07 -0300

    Revisando basico

--- src/AlgoritmoQuestoes/Questao06.java
+++ src/AlgoritmoQuestoes/Questao06.java
@@ -1,4 +1,12 @@
 package AlgoritmoQuestoes;
 
 public class Questao06 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+    }
 }
+
+
