c32314214dd7c29090e1e9bb9fe8118e93684385
aldok <aldo.santos.oliveira95@gmail.com>
2022-08-22 22:26:07 -0300

    Revisando basico

---
+++ src/Revisando/Questa04.java
@@ -0,0 +1,10 @@
+package Revisando;
+
+public class Questa04 {
+    public static void main(String[] args) {
+        String name = "Aldo";
+        System.out.println("Hello " + name);
+
+    }
+}
+
