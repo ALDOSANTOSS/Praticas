b421c487816cedc0140017e70e2932638e9d47fa
aldok <aldo.santos.oliveira95@gmail.com>
2022-08-27 17:07:16 -0300

    Revisando basico

---
+++ src/Revisando/Questao09.java
@@ -0,0 +1,12 @@
+package Revisando;
+
+public class Questao09 {
+    public static void main(String[] args) {
+        int numero = 10;
+        int numer01 = 5;
+        if( numero > numer01){
+            System.out.println("resultado");
+        }
+
+    }
+}
