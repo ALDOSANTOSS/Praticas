35fd4212a828482810ff5e869d3533c0258771be
aldok <aldo.santos.oliveira95@gmail.com>
2022-08-28 22:04:38 -0300

    Revisando basico

---
+++ src/Revisando/Questao10.java
@@ -0,0 +1,17 @@
+package Revisando;
+
+
+
+public class Questao10 {
+    public static void main(String[] args) {
+        int velocidade = 50;
+        int rada = 80;
+        if (velocidade > rada){
+            System.out.println("Mutado");
+        }
+        else {
+            System.out.println("certo");
+        }
+
+    }
+}
